b808fdb99f5d4c94a9916a50e7d2fd8ebd2b1270
Kittygomeowr <rkussery@gmail.com>
2017-10-14 08:25:29 -0600

    got hit working

--- src/com/skilldistillery/cards/blackjack/GameDriver.java
+++ src/com/skilldistillery/cards/blackjack/GameDriver.java
@@ -8,30 +8,36 @@ public class GameDriver {
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Welcome, gambler. What's your name?");
 		String name = kb.next();
-		System.out.println(name + ", huh? Well, let's get started.");
-
+		System.out.println(name + ", huh? Are you ready to play? (Y/N)");
+		String ready = kb.next();
 		int score = 0;
 
-		System.out.println("You are dealt:");
-		score = Hand.fillHand();
-		// play again loop
-		System.out.println("Would you like to hit, stay, or quit? (H/S/Q)");
-		String choice = kb.next();
-		do {
-			if (choice.equals("H")) {
-				score = Hand.hitMe(score);
-				System.out.println(score);
-				
-			} else if (choice.equals("S")) {
-				Table.stay();
+		if (ready.equals("Y")) {
+			System.out.println("You are dealt:");
+			score = Hand.fillHand();
+			String choice = null;
+			// play again loop
+			do {
+				System.out.println("Would you like to hit, stay, or quit? (H/S/Q)");
+				choice = kb.next();
+				if (choice.equals("H")) {
+					score = Hand.hitMe(score);
+					if (score > 21) {
+						continue;
+					}
+					System.out.println("Your score is: " + score);
 
-			}
+				} else {
+					Table.stay();
+					break;
+				}
 
-		} while (choice.equals("Q"));
-		System.out.println("A sore loser, eh? Well, we don't like your kind around these parts.");
-		kb.close();
+			} while (choice.equals("H"));
+		} else {
+			System.out.println("A sore loser, eh? Well, we don't like your kind around these parts.");
+			kb.close();
+		}
 	}
-
 }
 /*
  * Greeting / name Game prompts (stay, hold)
